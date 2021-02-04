233286983443693788da024ff59ce02315d49ebe
Renato Santos <renato_jfs@hotmail.com>
2021-02-04 00:16:11 -0300

    Context cart

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -3,6 +3,7 @@ import FeatherIcon from 'react-native-vector-icons/Feather';
 
 import { View } from 'react-native';
 
+// import { isIdentifier } from 'typescript';
 import {
   Container,
   ProductContainer,
@@ -39,23 +40,31 @@ const Cart: React.FC = () => {
   const { increment, decrement, products } = useCart();
 
   function handleIncrement(id: string): void {
-    // TODO
+    increment(id);
   }
 
   function handleDecrement(id: string): void {
-    // TODO
+    decrement(id);
   }
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const productSubtotal = product.price * product.quantity;
 
-    return formatValue(0);
+      return accumulator + productSubtotal;
+    }, 0);
+
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const productQuantity = product.quantity;
 
-    return 0;
+      return accumulator + productQuantity;
+    }, 0);
+
+    return total;
   }, [products]);
 
   return (
